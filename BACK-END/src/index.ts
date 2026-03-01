bf10911e1e0fdb21d4216b9223792ce3a9bbf9db
TackaIzHe <vitalia_mikhailov@mail.ru>
2026-03-01 21:30:14 +0000

    v-0.1.2 (update back-end) added https

--- BACK-END/src/index.ts
+++ BACK-END/src/index.ts
@@ -4,6 +4,9 @@ import setMiddleware from "./middleware/index"
 import index_router from "./routers/index"
 import ENV from "dotenv"
 import cookieParser from "cookie-parser";
+import fs from "fs"
+import https from "https"
+import path from "path";
 
 ENV.config({path: __dirname + "/../.env"})
 
@@ -20,7 +23,24 @@ setMiddleware(app)
 
 DbContext.initialize()
 .then(()=>{
-    app.listen(port,()=>{
-        console.log(`http://localhost:${port}`)
-    })
+    if (
+        fs.existsSync(path.join(__dirname, "../cert", "key.pem")) &&
+        fs.existsSync(path.join(__dirname, "../cert", "csr.pem")) &&
+        fs.existsSync(path.join(__dirname, "../cert", "cert.pem"))
+    )
+    {
+        const httpsServer = https.createServer({
+            key: fs.readFileSync(path.join(__dirname, "../cert", "key.pem")),
+            cert: fs.readFileSync(path.join(__dirname, "../cert", "cert.pem"))
+        }, app)
+
+        httpsServer.listen(port, ()=>{
+            console.log(`https://localhost:${port}`)
+        })
+    }
+    else{
+        app.listen(port,()=>{
+            console.log(`http://localhost:${port}`)
+        })
+    }
 })
